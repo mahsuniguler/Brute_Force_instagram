7d34b1b68513577820d3164293ad9ab2e30dba61
mahsuniguler <m.mahsuni.glr@gmail.com>
2023-10-06 18:14:20 +0300

    Düzenleme Yapıldı.

--- Brute_force.py
+++ Brute_force.py
@@ -1,11 +1,9 @@
 import random
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pyautogui
 import time
 
 username = input("Kullanıcı Adını gir: ")
